5b10e3483fc8918bffdae5c20333ff9bf379db70
Sail <151263555@qq.com>
2022-06-10 11:14:15 +0800

    v1.0.2

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import { isProperty } from "https://deno.land/x/is_property@v1.0.1/mod.ts";
-import { format } from "https://deno.land/std@0.142.0/node/util.ts";
+import { isProperty } from "https://deno.land/x/is_property/mod.ts";
+import { format } from "https://deno.land/std/node/util.ts";
 
 export { isProperty, format };
